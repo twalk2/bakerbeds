0205ede0e95064157fd6855daa471b572f44c2bc
twalk2 <2twalk2@gmail.com>
2020-06-09 18:52:42 -0600

    initial commit

---
+++ components/AppHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <nav class="bg-white shadow">
+      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+          <div class="flex">
+            <div class="flex-shrink-0 flex items-center">
+              <nuxt-link to="/" class="text-indigo-600 font-semibold">
+                <h1>Baker Beds</h1>
+              </nuxt-link>
+            </div>
+            <div class="hidden sm:ml-6 sm:flex">
+              <nuxt-link
+                to="/"
+                exact-active-class="border-indigo-500 text-gray-700"
+                class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out"
+              >Home</nuxt-link>
+              <nuxt-link
+                to="about"
+                exact-active-class="border-indigo-500 text-gray-700"
+                class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out"
+              >About</nuxt-link>
+              <nuxt-link
+                to="contact"
+                exact-active-class="border-indigo-500 text-gray-700"
+                class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out"
+              >Contact</nuxt-link>
+              <nuxt-link
+                to="product"
+                exact-active-class="border-indigo-500 text-gray-700"
+                class="ml-8 inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out"
+              >Product</nuxt-link>
+            </div>
+          </div>
+          <div class="hidden sm:ml-6 sm:flex sm:items-center">
+            <button
+              class="p-1 border-2 border-transparent text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:text-gray-500 focus:bg-gray-100 transition duration-150 ease-in-out"
+              aria-label="Notifications"
+            >
+              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader"
+};
+</script>
+
+<style scoped>
+</style>
